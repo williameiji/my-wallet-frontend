74b7891f10c0737504165c9cbc77220ff70de6ce
williameiji <william.fujimori@gmail.com>
2022-07-01 16:43:28 -0300

    fix: link to api

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,7 +1,7 @@
 const url = {
-	login: "http://localhost:5000/login",
-	signup: "http://localhost:5000/signup",
-	history: "http://localhost:5000/history",
+	login: "https://my-wallet-back-projeto-13.herokuapp.com/login",
+	signup: "https://my-wallet-back-projeto-13.herokuapp.com/signup",
+	history: "https://my-wallet-back-projeto-13.herokuapp.com/history",
 };
 
 export default url;
